41b2431ba38d1147bb80ea59285bd7ac2c8aadcc
unknown <lhufancia@gbox.adnu.edu.ph>
2025-10-29 17:16:50 +0800

    fixed routes for auth pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
 import OwnerInformation from "./pages/owners/Owner_Information";
@@ -8,12 +8,12 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/owner-information" element={<OwnerInformation />} />
         <Route path="/owner-submission" element={<OwnerSubmission />} />
       </Routes>
-
     </Router>
   );
 }
